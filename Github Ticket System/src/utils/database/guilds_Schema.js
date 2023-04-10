523e8dd73eae942310fbbd731b62808343e437dd
Eray <129968185+deverays@users.noreply.github.com>
2023-04-10 20:34:46 +0300

    Delete Github Ticket System/src directory

--- Github Ticket System/src/utils/database/guilds_Schema.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose"
-
-export default mongoose.model("guilds", new mongoose.Schema({
-    guild_id: { type: String, unique: true, required: true },
-    test: { type: String, default: "test" },
-    ticket_category_id: { type: String, default: null },
-    ticket_role_id: { type: String, required: true },
-}))
--- Github Ticket System/src/utils/event-utils/modal_submit.js
+++
@@ -1,9 +0,0 @@
-import suggestion from "../modals/suggestion.js"
-import ticket from "../modals/ticket.js"
-
-export default interaction => {
-
-    if (interaction.customId == "suggestion") suggestion(interaction)
-    if (interaction.customId == "ticketModals") ticket(interaction)
-
-}
